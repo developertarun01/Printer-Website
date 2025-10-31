41cec0af84f7ca629adeb1e46c750716940a1fea
developertarun01 <tarunbusinessmail@gmail.com>
2025-10-31 13:24:57 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Auto slide every 1 second
+setInterval(nextSlide, 1500);
